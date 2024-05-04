ea8e540f31fe340c51446cb3258acc1603da017b
Jackson Naumann <jackson.naumann@ndsu.edu>
2024-05-04 17:47:34 -0500

    uhhhhh templates

--- eventcalendar/urls.py
+++ eventcalendar/urls.py
@@ -3,4 +3,5 @@ from eventcalendar import views
 
 urlpatterns = [
     path('', views.home, name="home"  )
+    path('calendar/', views.c)
 ]
--- eventcalendar/views.py
+++ eventcalendar/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from models import Event, Game, Lan, Tournament
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def calendar(request):
+    return render(request, 'calendar.html')
