e6b2a8472a52a3b0256441e284013bb270d5f2a0
Jackson Naumann <jackson.naumann@ndsu.edu>
2024-05-04 17:15:57 -0500

    Base templates and views

--- LoLClubWebsite/urls.py
+++ LoLClubWebsite/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('eventcalendar/', include('eventcalendar.urls')),
     path('statsviewer/', include('statsviewer.urls')),
+    path('', include ('eventcalendar.urls'))
 ]
--- eventcalendar/urls.py
+++ eventcalendar/urls.py
@@ -1,1 +1,6 @@
-urlpatterns = []
+from django.urls import path, include
+from eventcalendar import views
+
+urlpatterns = [
+    path('', views.home, name="home"  )
+]
