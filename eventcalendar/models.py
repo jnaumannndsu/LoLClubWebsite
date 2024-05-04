1ea97479bb07069a6e2951801f9e735d87be0c86
Jackson Naumann <jackson.naumann@ndsu.edu>
2024-05-04 14:25:47 -0500

    Added models

--- eventcalendar/models.py
+++ eventcalendar/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Event(models.Model):
+    """Model for calendar events."""
+    eventID = models.BigAutoField(primary_key=True)
+    startTime = models.DateTimeField()
+    endTime = models.DateTimeField()
+    eventName = models.CharField(default="New Event", max_length=160)
+
+    def __str__(self):
+        return self.eventName
+    
+    def get_absolute_url(self):
+        return reverse("event_detail", kwargs={"pk": self.pk})
+    
+
+
+
+class Lan(models.Model):
+    event = models.OneToOneField('Event', on_delete=models.CASCADE, null=False, primary_key=True)
+    location = models.CharField(max_length=320)
+
+class Tournament(models.Model):
+    event = models.OneToOneField('Event', on_delete=models.CASCADE, null=False, primary_key=True)
+    STREAM_STATUS = (
+        ('m', 'Main'),
+        ('a', 'Alt'),
+        ('n', 'None'),
+    )
+    status = models.CharField(max_length=1, choices=STREAM_STATUS, blank=True, default='n', help_text='Where streamed')
+    signup = models.URLField()
+
+class Game(models.Model):
+    event = models.OneToOneField('Event', on_delete=models.CASCADE, null=False, primary_key=True)
+    TEAM_FIELDS = (
+        ('v', 'Varsity'),
+        ('j', 'Junior Varsity'),
+        ('c', 'C Team'),
+        ('d', 'D Team'),
+        ('o', 'Other'),
+    )
+    team = models.CharField(max_length=1, choices = TEAM_FIELDS, blank=True, help_text= 'Team playing')
+    LEAGUE_FIELDS = (
+        ('c', 'CCL'),
+        ('n', 'NECC'),
+        ('e', 'NACE'),
+        ('o', 'Other'),
+    )
+    league = models.CharField(max_length=1, choices=LEAGUE_FIELDS, blank=True, help_text='What League are they competing in')
+    STREAM_STATUS = (
+        ('m', 'Main'),
+        ('a', 'Alt'),
+        ('n', 'None'),
+    )
+    status = models.CharField(max_length=1, choices=STREAM_STATUS, blank=True, default='n', help_text='Where streamed')
+    
+
--- statsviewer/models.py
+++ statsviewer/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Game(models.Model):
+    gameid = models.BigAutoField(primary_key=True)
+    winner = models.BooleanField(null=False)
+
+class PlayerInstance(models.Model):
+    instanceid = models.BigAutoField(primary_key=True)
+    game = models.ForeignKey('Game', on_delete=models.RESTRICT)
+    playerlocation = models.IntegerField(null=False, validators=[MaxValueValidator(9), MinValueValidator(0)])
+    ROLE_FIELDS = (
+        ('t', 'TOP'),
+        ('j', 'JUNGLE'),
+        ('m', 'MID'),
+        ('a', 'ADC'),
+        ('s', 'SUP'),
+    )
+    role = models.CharField(max_length=1, choices=ROLE_FIELDS, help_text='What role did they play')
+    kills = models.IntegerField()
+    deaths = models.IntegerField()
+    assists = models.IntegerField()
+    gold = models.IntegerField()
+    damage = models.IntegerField()
+    ign = models.CharField(max_length=30)
+    player = models.ForeignKey('Player', on_delete=models.RESTRICT)
+
+class Player(models.Model):
+    playerid = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=60)
