7fcfa5568e1433df008781b8268599421cc65a44
Jackson Naumann <jackson.naumann@ndsu.edu>
2024-05-05 21:41:22 -0500

    player detail view and functionality stuff yeah

--- statsviewer/views.py
+++ statsviewer/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views import generic
 from statsviewer.models import *
 
@@ -28,9 +28,32 @@ def gohome(request):
     return redirect(reverse('home', current_app='eventcalendar'))
         
             
+# PLAYER DETAIL VIEW WE WANT TO HAVE
+# PLAYER STATS FROM BEFORE (PIS)
+# GAMES WIN LOSS TOTAL
 
 class playerDetailView(generic.DetailView):
     model = Player
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        player = self.object
+        dataList = [player,0,0,0,0,0,0,0]
+        instances = PlayerInstance.objects.filter(player = player.playerid)
+        #swag method
+        for pi in instances:
+            game = Game.objects.filter(pk = pi.game.gameid)  
+            if pi.playerlocation <= 4 and game[0].winner == True or pi.playerlocation > 4 and game[0].winner == False:
+                dataList[6] += 1
+            else:
+                dataList[7] += 1
+            dataList[1] += pi.kills
+            dataList[2] += pi.deaths
+            dataList[3] += pi.assists
+            dataList[4] += pi.gold
+            dataList[5] += pi.damage
+        context = {'list' : dataList}
+        return context
 
 class gameListView(generic.ListView):
     model = Game
