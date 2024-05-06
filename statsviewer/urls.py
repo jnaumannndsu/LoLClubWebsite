936ef2eee97909165a226eebf7feb1a225940204
BuildTools <unconfigured@null.spigotmc.org>
2024-05-05 22:43:50 -0500

    Game List

--- statsviewer/urls.py
+++ statsviewer/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('stats_home', views.stats, name = 'stats-home'),
     path('stats_home/player_list/', views.playerListView, name = 'player-list'),
     path('stats_home/player/<int:pk>', views.playerDetailView.as_view(), name = 'player-detail'),
+    path('stats_home/game_list/', views.gameListView, name = 'game-list'),
     path('', views.gohome, name = 'go-home'),
 ]
 
